cf5557703181dd2043fe4b4ebf9e9f29bc5faa66
vm-laptop <wolfmariev@gmail.com>
2020-06-01 20:47:33 +0500

    separated webpack config

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true,
+    },
+    // ecmaVersion: 11,
+    // sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    '@typescript-eslint/no-var-requires': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['./src'],
+      },
+    },
+  },
+};
--- webpack/client.config.js
+++ webpack/client.config.js
@@ -1,47 +1,22 @@
 const path = require('path');
-const webpack = require('webpack');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const baseConfig = require('./base.config');
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: 'production',
-  entry: path.resolve(__dirname, '../', 'src', 'client', 'app.tsx'),
-  output: {
-    path: path.resolve(__dirname, '../' ,'dist'),
-    filename: 'index.js',
-  },
   module: {
     rules: [
-        {
-            test: /\.(js|jsx|tsx|ts)$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        },
-        {
-            test: /\.s[ac]ss$/i,
-            exclude: /node_modules/,
-            use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader'],
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
         }),
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       },
     ],
   },
@@ -50,13 +25,8 @@ module.exports = {
     new ExtractTextPlugin('style.css'),
     // new webpack.IgnorePlugin(/jsdom$/),
     new HtmlWebPackPlugin({
-        template: path.resolve(__dirname, '../', 'html', 'index.html'), //'./src/index.html',
-        title: 'Webpack server'
+      template: path.resolve(__dirname, '../', 'html', 'index.html'),
+      title: 'Webpack server',
     }),
-
-
   ],
-  resolve: {
-    extensions: ['.jsx', '.tsx', '.ts', '.js']
-  }
-}
+});
